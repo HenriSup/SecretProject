6ac3f16411d6ad52d3bf4f30db192af0c8e2e71e
Henri Journel <Henri Journel>
2017-03-15 10:09:48 +0100

    Edit buttonBehavior

--- assets/Scripts (12)/ChoiceButtonBehavior (40)/script.ts
+++ assets/Scripts (12)/ChoiceButtonBehavior (40)/script.ts
@@ -1,19 +1,34 @@
 class ChoiceButtonBehavior extends Sup.Behavior {
   
   isClickable:boolean = false
-  shouldFlicker:boolean = false
+  isOn=true
   timer:number = 0
   flickerFrame = 0
+  spriteRenderer:Sup.SpriteRenderer 
   awake() {
+    this.spriteRenderer=this.actor.spriteRenderer
     this.actor.spriteRenderer.setAnimation("ON")
   }
 
   update() {
-    Sup.Input.getMousePosition()
+    Sup.log(this.spriteRenderer.getAnimationFrameCount())
+    if (this.spriteRenderer.getAnimation()=="Flicker" && (this.spriteRenderer.getAnimationFrameIndex()>=this.spriteRenderer.getAnimationFrameCount()-1)){
+      if(this.isOn){
+        this.spriteRenderer.setAnimation("ON")
+      } else {
+        this.spriteRenderer.setAnimation("OFF")
+      }
+    }
+    if (this.timer>=this.flickerFrame){
+      this.flicker()
+      this.timer=0
+      this.flickerFrame = Sup.Math.Random.integer(0,300)
+    }
+    this.timer++
   }
   
   flicker(){
-    this.actor.spriteRenderer.setAnimation("Flicker")
+    this.actor.spriteRenderer.setAnimation("Flicker",false)
   }
 }
 Sup.registerBehavior(ChoiceButtonBehavior);
